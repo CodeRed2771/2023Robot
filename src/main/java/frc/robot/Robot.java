c6c46ed87013322c0c1accc3e89a6231121e2df8
Elisha B <ejbuff16@gmail.com>
2023-02-18 15:01:29 -0500

    worked

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -218,12 +218,12 @@ public class Robot extends TimedRobot {
         if (gamepad2.getRightShoulder().getAsBoolean()){
             if (gamepad2.getLeftBumper()){
                 Intake.deploy();
-                Intake.reverse(0.5);
+                Intake.reverse(0.75);
                 
             } 
             else{
                 Intake.deploy();
-                Intake.run(0.5);
+                Intake.run(0.75);
             }
         }
         if (gamepad2.getRightBumper()){
