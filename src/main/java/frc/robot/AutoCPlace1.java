e06281f8714770fe9d3dcd229bc7fd4e4efa3ed3
Elisha B <ejbuff16@gmail.com>
2023-02-23 19:41:39 -0500

    Finished Auto

--- src/main/java/frc/robot/AutoCPlace1.java
+++ src/main/java/frc/robot/AutoCPlace1.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Arm.bistablePresets;
 import frc.robot.Arm.shoulderPresets;
 
-public class AutoCommunity extends AutoBaseClass{
+public class AutoCPlace1 extends AutoBaseClass{
 
 	public void start() {
 		super.start();
@@ -32,15 +32,30 @@ public class AutoCommunity extends AutoBaseClass{
                 case 3:
                     break;
                 case 4:
-                    Arm.presetLift(shoulderPresets.PLACING);
+                    Arm.presetLift(shoulderPresets.PLACING_HIGH);
                     setTimerAndAdvanceStep(750);
                     break;
                 case 5:
                     break;
                 case 6:
                     Arm.presetExtend(bistablePresets.HIGH);
-                    
-                case 9999:
+                    setTimerAndAdvanceStep(1500);
+                case 7:
+                    break;
+                case 8:
+                    Claw.openClaw();
+                    setTimerAndAdvanceStep(750);
+                case 9:
+                    break;
+                case 10:
+                    Claw.closeClaw();
+                    Arm.presetExtend(bistablePresets.RETRACTED);
+                    setTimerAndAdvanceStep(2000);
+                case 11:
+                    break;
+                case 12:
+                    driveInches(250, 0, 0.8);
+                case 13:
                     stop();
                     break;
             }
