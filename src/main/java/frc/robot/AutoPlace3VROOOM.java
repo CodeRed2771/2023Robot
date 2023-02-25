e2a501709757e88b152d49a54ece6f5061287e20
Elisha B <ejbuff16@gmail.com>
2023-02-25 16:48:21 -0500

    auto made

---
+++ src/main/java/frc/robot/AutoPlace3VROOOM.java
@@ -0,0 +1,69 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Arm.bistablePresets;
+import frc.robot.Arm.shoulderPresets;
+
+public class AutoCPlace1 extends AutoBaseClass{
+
+	public void start() {
+		super.start();
+	}
+    
+    public void stop() {
+        super.stop();
+    }
+
+    @Override
+    public void tick() {
+        if (isRunning()) {
+            SmartDashboard.putNumber("Auto Step", getCurrentStep());
+            switch (getCurrentStep()) {
+                case 0:
+                    Claw.closeClawA();
+                    setTimerAndAdvanceStep(250);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    Arm.presetExtend(bistablePresets.RETRACTED);
+                    setTimerAndAdvanceStep(1250);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    Arm.presetLift(shoulderPresets.PLACING_HIGH);
+                    setTimerAndAdvanceStep(750);
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    Arm.presetExtend(bistablePresets.HIGH);
+                    setTimerAndAdvanceStep(1500);
+                case 7:
+                    break;
+                case 8:
+                    Claw.openClawA();
+                    setTimerAndAdvanceStep(750);
+                case 9:
+                    break;
+                case 10:
+                    Claw.closeClawA();
+                    Arm.presetExtend(bistablePresets.RETRACTED);
+                    setTimerAndAdvanceStep(2000);
+                case 11:
+                    break;
+                case 12:
+                    driveInches(250, 0, 0.8);
+                    setTimerAndAdvanceStep(6000);
+                case 13:
+                    if(driveCompleted())
+                        advanceStep();
+                case 14:
+                    stop();
+                    break;
+            }
+        }
+    }
+
+}
