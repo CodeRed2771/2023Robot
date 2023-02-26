44ac252bf2539f3fa9e689db38635fe70bc0582c
Elisha B <ejbuff16@gmail.com>
2023-02-26 14:04:10 -0500

    Worked on 3 element, (All low) auto.
    We might want to make an extension so if there is enough time left in auto, we place on a higher position and not just on the floor. We also need to test it.

--- src/main/java/frc/robot/AutoPlace3VROOOM.java
+++ src/main/java/frc/robot/AutoPlace3VROOOM.java
@@ -1,10 +1,14 @@
 package frc.robot;
-
+//robot is 31 in wide and each side is 15.5 (With Bumpers)
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Arm.bistablePresets;
 import frc.robot.Arm.shoulderPresets;
 
-public class AutoCPlace1 extends AutoBaseClass{
+public class AutoPlace3VROOOM extends AutoBaseClass{
+
+    //TODO: TUNE, ADD MULTIPLIER FOR DIFFERENT POSITIONS
+
+    int postionMultiplier = 0;
 
 	public void start() {
 		super.start();
@@ -19,47 +23,140 @@ public class AutoCPlace1 extends AutoBaseClass{
         if (isRunning()) {
             SmartDashboard.putNumber("Auto Step", getCurrentStep());
             switch (getCurrentStep()) {
-                case 0:
-                    Claw.closeClawA();
-                    setTimerAndAdvanceStep(250);
+                case 0://eject preload
+                    LiveBottom.eject();
+                    setTimerAndAdvanceStep(1250);
                     break;
                 case 1:
                     break;
-                case 2:
-                    Arm.presetExtend(bistablePresets.RETRACTED);
-                    setTimerAndAdvanceStep(1250);
+                case 2://slide to pickup position
+                    driveInches(23.2*postionMultiplier, 90, 0.8);//needs to be tuned
+                    setTimerAndAdvanceStep(1000);
                     break;
                 case 3:
+                    if(driveCompleted())
+                        advanceStep();
                     break;
-                case 4:
-                    Arm.presetLift(shoulderPresets.PLACING_HIGH);
-                    setTimerAndAdvanceStep(750);
+                case 4://go to pickup
+                    driveInches(216, 0, 1);
+                    setTimerAndAdvanceStep(2000);
                     break;
                 case 5:
+                    if(driveCompleted())
+                        advanceStep();
                     break;
-                case 6:
-                    Arm.presetExtend(bistablePresets.HIGH);
+                case 6://run intake
+                    Intake.run(1);
+                    break;
+                case 7://pickup
+                    driveInches(12, 0, 0.2);
                     setTimerAndAdvanceStep(1500);
-                case 7:
                     break;
                 case 8:
-                    Claw.openClawA();
-                    setTimerAndAdvanceStep(750);
-                case 9:
+                    if(driveCompleted())
+                        advanceStep();
                     break;
-                case 10:
-                    Claw.closeClawA();
-                    Arm.presetExtend(bistablePresets.RETRACTED);
+                case 9://make element ready to eject
+                    LiveBottom.forward();
+                    break;
+                case 10://stop intake and go to place
+                    Intake.stop();
+                    driveInches(-224, 0, 1);
                     setTimerAndAdvanceStep(2000);
-                case 11:
                     break;
-                case 12:
-                    driveInches(250, 0, 0.8);
-                    setTimerAndAdvanceStep(6000);
+                case 11:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 12://turn live bottom off and align for placing
+                    LiveBottom.off();
+                    driveInches(-5, 90, 0.8);//needs to be tuned
+                    setTimerAndAdvanceStep(1000);
+                    break;
                 case 13:
                     if(driveCompleted())
                         advanceStep();
-                case 14:
+                    break;
+                case 14://get ready for ejecting the elemnt
+                    driveInches(-4, 0, 0.5);
+                    setTimerAndAdvanceStep(1000);
+                case 15:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 16://eject the element
+                    LiveBottom.eject();
+                    setTimerAndAdvanceStep(750);
+                    break;
+                case 17:
+                    break;
+                case 18://drive to 3rd elemnet
+                    driveInches(216, 90, 1);//might need slide before
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 19:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 20://start intake and align to pick up element
+                    Intake.run(1);
+                    driveInches(-20, 90, 1);//needs to be tuned
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 21:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 22://pick up element
+                    driveInches(12, 0, 0.2);
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 23:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 24://stop intake, start live bottom, and drive to go back
+                    Intake.stop();
+                    LiveBottom.forward();
+                    driveInches(20, 90, 1);//needs to be tuned
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 25:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 26://drive back
+                    driveInches(-224, 0, 1);
+                    setTimerAndAdvanceStep(2000);
+                    break;
+                case 27:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 28://turn live bottom off, align horizontally
+                    LiveBottom.off();
+                    driveInches(5, 90, 0.5);//needs to be tuned
+                    setTimerAndAdvanceStep(1250);
+                    break;
+                case 29:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 30://drive back a lill' more
+                    driveInches(-4, 0, 0.5);//needs to be tuned
+                    setTimerAndAdvanceStep(1250);
+                    break;
+                case 31:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 32://eject elemnt
+                    LiveBottom.eject();
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 33:
+                    break;
+                case 34:
                     stop();
                     break;
             }
