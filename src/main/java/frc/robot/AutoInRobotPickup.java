e7cda6082656d4611c80ce1f4a9ebc8f82cf4dd4
StormyHugo426 <ejbuff16@gmail.com>
2023-03-09 16:11:50 -0800

    new auto pickup

---
+++ src/main/java/frc/robot/AutoInRobotPickup.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Arm.extenderPresets;
+import frc.robot.Arm.shoulderPresets;
+
+public class AutoInRobotPickup extends AutoBaseClass {
+
+
+    public AutoInRobotPickup () {
+
+    }
+
+    public void start() {
+        super.start();
+    }
+
+    public void stop() {
+        super.stop();
+    }
+
+    @Override
+    public void tick() {
+        if (isRunning()) {
+            DriveAuto.tick();
+            SmartDashboard.putNumber("Auto Step", getCurrentStep());
+            switch (getCurrentStep()) {
+                case 0:
+                    Arm.presetExtend(extenderPresets.RETRACTED);
+                    setTimerAndAdvanceStep(2000);
+                    break;
+                case 1:
+                    if(extensionCompleted)
+                        advanceStep();
+                case 5:
+                    stop();
+                    break;
+            }
+        }
+    }
+}
