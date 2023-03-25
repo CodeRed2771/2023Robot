bd76d5732b121ec74c08c6774ef1d7247fb57d5d
StormyHugo426 <ejbuff16@gmail.com>
2023-03-25 11:57:36 -0400

    worked on auto P3 ready for testing (Starting now)

--- src/main/java/frc/robot/AutoPlace3VROOOM.java
+++ src/main/java/frc/robot/AutoPlace3VROOOM.java
@@ -6,13 +6,11 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Arm.extenderPresets;
 import frc.robot.Arm.shoulderPresets;
+import frc.robot.AutoBaseClass.Position;
 
 public class AutoPlace3VROOOM extends AutoBaseClass{
-    int positionMultiplier = 0;
-    int posRLOffset = 0;
-    int posRROffset = 0;
-    int posBLOffset = 0;
-    int posBROffset = 0;
+
+    byte positionMultiplier = 1;
 
 	public void start() {
 		super.start();
@@ -27,19 +25,28 @@ public class AutoPlace3VROOOM extends AutoBaseClass{
         if (isRunning()) {
             SmartDashboard.putNumber("Auto Step", getCurrentStep());
             switch (getCurrentStep()) {
-                case 0://eject preload and set position multiplier
-                    positionMultiplier = 1;
-                    if(DriverStation.getAlliance() == Alliance.Blue)
-                        positionMultiplier = positionMultiplier * -1;
-                    if(robotPosition() == Position.RIGHT)
-                        positionMultiplier = positionMultiplier * -1;
+                case 0://eject preload
                     LiveBottom2.backward();
                     setTimerAndAdvanceStep(1250);
                     break;
-                case 1:
-                    break;
+                case 1://set position stuff
+                if(DriverStation.getAlliance() == Alliance.Blue)
+                    positionMultiplier = -1;
+                else 
+                    positionMultiplier = 1;
+                
+                if(robotPosition() == Position.RIGHT && DriverStation.getAlliance() == Alliance.Blue)
+                    setStep(37);
+                if(robotPosition() == Position.RIGHT && DriverStation.getAlliance() == Alliance.Blue)
+                    setStep(37);
+                else if(robotPosition() == Position.CENTER)
+                    setStep(72);
+                else
+                    advanceStep();
+                break;
+                    
                 case 2://slide to pickup position
-                    driveInches(43.75*positionMultiplier, 90, 0.8);//TUNE LINE??
+                    driveInches(6*positionMultiplier, 90, 0.8);//TUNE LINE
                     setTimerAndAdvanceStep(1000);
                     break;
                 case 3:
@@ -81,16 +88,17 @@ public class AutoPlace3VROOOM extends AutoBaseClass{
                         advanceStep();
                     break;
                 case 12://align for placing
-                    driveInches(-8*positionMultiplier, 90, 0.8);//TUNE LINE
+                    driveInches(-18*positionMultiplier, 90, 0.8);//TUNE LINE
                     setTimerAndAdvanceStep(1000);
                     break;
                 case 13:
                     if(driveCompleted())
                         advanceStep();
                     break;
-                case 14://get ready for ejecting the elemnt
+                case 14://get ready for ejecting the element
                     driveInches(-6, 0, 0.5);
                     setTimerAndAdvanceStep(1000);
+                    break;
                 case 15:
                     if(driveCompleted())
                         advanceStep();
@@ -102,7 +110,7 @@ public class AutoPlace3VROOOM extends AutoBaseClass{
                 case 17:
                     break;
                 case 18://drive back to drive to new element
-                    driveInches(8*positionMultiplier, 90, 0.8);//TUNE LINE
+                    driveInches(18*positionMultiplier, 90, 0.8);//TUNE LINE
                     setTimerAndAdvanceStep(1000);
                     break;
                 case 19:
@@ -146,7 +154,7 @@ public class AutoPlace3VROOOM extends AutoBaseClass{
                         advanceStep();
                     break;
                 case 28://drive back
-                    driveInches(-224, 0, 1);
+                    driveInches(-224*positionMultiplier, 0, 1);
                     setTimerAndAdvanceStep(2000);
                     break;
                 case 29:
@@ -154,7 +162,7 @@ public class AutoPlace3VROOOM extends AutoBaseClass{
                         advanceStep();
                     break;
                 case 30://turn live bottom off, align horizontally
-                    driveInches(5*positionMultiplier, 90, 0.5);//TUNE LINE
+                    driveInches(40*positionMultiplier, 90, 0.5);//TUNE LINE
                     setTimerAndAdvanceStep(1250);
                     break;
                 case 31:
@@ -176,6 +184,150 @@ public class AutoPlace3VROOOM extends AutoBaseClass{
                 case 35:
                     break;
                 case 36:
+                    setStep(72);
+                    break;
+                case 37://slide to pickup position
+                    driveInches(6*positionMultiplier, 90, 0.8);//TUNE LINE
+                    setTimerAndAdvanceStep(1000);
+                    break;
+                case 38:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 39://go to pickup
+                    LiveBottom2.forward();
+                    driveInches(216, 0, 1);
+                    setTimerAndAdvanceStep(2000);
+                    break;
+                case 40:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 41://run intake
+                    Intake.deploy();
+                    Intake.run(1);
+                    break;
+                case 42://pickup
+                    driveInches(12, 0, 0.2);
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 43:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 44://make element ready to eject
+                    //unneeded
+                    break;
+                case 45://stop intake and go to place
+                    Intake.stop();
+                    Intake.retract();
+                    driveInches(-224, 0, 1);
+                    setTimerAndAdvanceStep(3000);
+                    break;
+                case 46:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 47://align for placing
+                    driveInches(-18*positionMultiplier, 90, 0.8);//TUNE LINE
+                    setTimerAndAdvanceStep(1000);
+                    break;
+                case 48:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 49://get ready for ejecting the element
+                    driveInches(-6, 0, 0.5);
+                    setTimerAndAdvanceStep(1000);
+                    break;
+                case 50:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 51://eject the element
+                    LiveBottom2.backward();
+                    setTimerAndAdvanceStep(750);
+                    break;
+                case 52:
+                    break;
+                case 53://drive back to drive to new element
+                    driveInches(18*positionMultiplier, 90, 0.8);//TUNE LINE
+                    setTimerAndAdvanceStep(1000);
+                    break;
+                case 54:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 55://drive to 3rd elemnet
+                    LiveBottom2.forward();
+                    driveInches(216, 0, 1);
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 56:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 57://start intake and align to pick up element
+                    Intake.deploy();
+                    Intake.run(1);
+                    driveInches(-48*positionMultiplier, 90, 1);//TUNE LINE??
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 58:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 59://pick up element
+                    driveInches(12, 0, 0.2);
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 60:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 61://stop intake, start live bottom, and drive to go back
+                    Intake.stop();
+                    driveInches(48*positionMultiplier, 90, 1);//TUNE LINE
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 62:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 63://drive back
+                    driveInches(-224*positionMultiplier, 0, 1);
+                    setTimerAndAdvanceStep(2000);
+                    break;
+                case 64:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 65://turn live bottom off, align horizontally
+                    driveInches(40*positionMultiplier, 90, 0.5);//TUNE LINE
+                    setTimerAndAdvanceStep(1250);
+                    break;
+                case 66:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 67://drive back a lill' more
+                    driveInches(-4, 0, 0.5);//TUNE LINE
+                    setTimerAndAdvanceStep(1250);
+                    break;
+                case 68:
+                    if(driveCompleted())
+                        advanceStep();
+                    break;
+                case 69://eject elemnt
+                    LiveBottom2.backward();
+                    setTimerAndAdvanceStep(1500);
+                    break;
+                case 70:
+                    break;
+                case 71:
+                    advanceStep();
+                    break;
+                case 72:
                     LiveBottom2.forward();
                     stop();
                     break;
