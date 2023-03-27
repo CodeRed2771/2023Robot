9fa7d4df995f40bfc1262ff7946f67b1c7c25071
StormyHugo426 <ejbuff16@gmail.com>
2023-03-27 19:01:10 -0400

    removed errors

--- src/main/java/frc/robot/AutoCPlace2.java
+++ src/main/java/frc/robot/AutoCPlace2.java
@@ -49,13 +49,13 @@ public class AutoCPlace2 extends AutoBaseClass{//didn't change much
                 case 5:
                     break;
                 case 6:
-                    if(getAlliance() == Alliance.Red && getAutoPosition() == Position.LEFT)
+                    if(getAlliance() == Alliance.Red && robotPosition() == Position.LEFT)
                         driveInches(driveDistRedL, wheelAngleRedL, 0.9);
-                    if(getAlliance() == Alliance.Red && getAutoPosition() == Position.RIGHT)
+                    if(getAlliance() == Alliance.Red && robotPosition() == Position.RIGHT)
                         driveInches(driveDistRedR, wheelAngleRedR, 0.9);
-                    if(getAlliance() == Alliance.Blue && getAutoPosition() == Position.LEFT)
+                    if(getAlliance() == Alliance.Blue && robotPosition() == Position.LEFT)
                         driveInches(driveDistBlueL, wheelAngleBlueL, 0.9);
-                    if(getAlliance() == Alliance.Blue && getAutoPosition() == Position.RIGHT)
+                    if(getAlliance() == Alliance.Blue && robotPosition() == Position.RIGHT)
                         driveInches(driveDistBlueR, wheelAngleBlueR, 0.9);
                     setTimerAndAdvanceStep(4000);
                     break;
@@ -98,7 +98,6 @@ public class AutoCPlace2 extends AutoBaseClass{//didn't change much
                 case 13:
                     break;
                 case 14:
-                    LiveBottom2.
                     stop();
                     break;
             }
